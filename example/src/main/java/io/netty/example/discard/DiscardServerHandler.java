09a6460e2c579f3d620c7db1052e663f4deed28c
hongkai.wang <whk.private@gmail.com>
2018-09-29 10:39:26 +0800

    对最简单的discard做收到的信息输出

--- example/src/main/java/io/netty/example/discard/DiscardServerHandler.java
+++ example/src/main/java/io/netty/example/discard/DiscardServerHandler.java
@@ -15,6 +15,7 @@
  */
 package io.netty.example.discard;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
@@ -26,6 +27,11 @@ public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
     @Override
     public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
         // discard
+        try{
+            System.out.println(((ByteBuf)msg).toString(io.netty.util.CharsetUtil.US_ASCII));
+        }finally {
+
+        }
     }
 
     @Override
